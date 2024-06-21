00404e5263cc159cb2620e36772a7fc3be62a69e
chaitu328 <konakallachaitanya@gmail.com>
2024-06-21 18:51:53 +0530

    fifth commit

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from "../Logo/Logo"
+import Logo from "../Logo/Logo";
 import SearchBar from '../SearchBar/SearchBar';
 import Button from '../Button/Button';
 import styles from './NavBar.module.css';
 
-const Navbar = () => {
+const NavBar = () => {
     return (
         <nav className={styles.navbar}>
+            <div className={styles.logoWrapper}>
                 <Logo />
+            </div>
             <SearchBar placeholder="Search a song of your choice" />
             <Button text="Give Feedback" />
         </nav>
     );
 };
 
-export default Navbar;
+export default NavBar;
